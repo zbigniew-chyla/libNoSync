110841e26e9afd38be27f6fd97db45afcd58167d
Zbigniew Chyla <zbigniew.chyla@gmail.com>
2018-05-10 22:37:54 +0200

    fd_utils: minor coding style unification
    1. Avoid unnecessary assignment of function result to variable before
    return - for consistency with other code (e.g. net-utils).
    2. Reorder *buffer/*size parameters in read_some_bytes_using_tmp_buffer
    such that size of the buffer immediately follows pointer to the buffer
    (which is commonly used convention everywhere) and rename *size param
    to match the buffer parameter name.
    
    No functional changes.

--- src/nosync/fd-utils.cc
+++ src/nosync/fd-utils.cc
@@ -22,19 +22,16 @@ namespace
 constexpr auto max_local_buffer_size = 8192U;
 
 
-result<string> read_some_bytes_using_tmp_buffer(int fd, size_t max_size, char *tmp_buffer)
+result<string> read_some_bytes_using_tmp_buffer(int fd, char *tmp_buffer, size_t tmp_buffer_size)
 {
     ssize_t read_res;
     do {
-        read_res = ::read(fd, tmp_buffer, max_size);
+        read_res = ::read(fd, tmp_buffer, tmp_buffer_size);
     } while (read_res == -1 && errno == EINTR);
 
-    auto res =
-        read_res >= 0
-            ? make_ok_result(string(tmp_buffer, static_cast<size_t>(read_res)))
-            : make_raw_error_result_from_errno();
-
-    return res;
+    return read_res >= 0
+        ? make_ok_result(string(tmp_buffer, static_cast<size_t>(read_res)))
+        : make_raw_error_result_from_errno();
 }
 
 }
@@ -43,10 +40,11 @@ result<string> read_some_bytes_using_tmp_buffer(int fd, size_t max_size, char *t
 result<string> read_some_bytes_from_fd(int fd, size_t max_size)
 {
     return read_some_bytes_using_tmp_buffer(
-        fd, max_size,
+        fd,
         max_size <= max_local_buffer_size
             ? array<char, max_local_buffer_size>().data()
-            : make_unique<char[]>(max_size).get());
+            : make_unique<char[]>(max_size).get(),
+        max_size);
 }
 
 
@@ -57,12 +55,9 @@ result<size_t> write_some_bytes_to_fd(int fd, string_view data)
         write_res = ::write(fd, data.data(), data.size());
     } while (write_res == -1 && errno == EINTR);
 
-    auto res =
-        write_res >= 0
-            ? make_ok_result(static_cast<size_t>(write_res))
-            : make_raw_error_result_from_errno();
-
-    return res;
+    return write_res >= 0
+        ? make_ok_result(static_cast<size_t>(write_res))
+        : make_raw_error_result_from_errno();
 }
 
 }
