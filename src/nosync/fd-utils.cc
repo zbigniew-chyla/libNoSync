8a3da778d5bff82e22391698fb682ff4a2536a0f
Zbigniew Chyla <zbigniew.chyla@gmail.com>
2018-05-10 22:37:54 +0200

    fd_utils: add func for reading into existing buffer
    Add a variant of read_some_bytes_from_fd function that reads data to
    provided buffer and returns the number of bytes read (not the data
    itself) as result<>.
    
    We'll need this variant in nosync code.
    
    The new variant is the thinnest wrapper around read() that adds only:
     - EINTR handling (retrying interrupted call),
     - error reporting via result<> (instead of -1 return value + errno).
    
    The implementation is extracted from read_some_bytes_using_tmp_buffer.

--- src/nosync/fd-utils.cc
+++ src/nosync/fd-utils.cc
@@ -3,6 +3,7 @@
 #include <cerrno>
 #include <memory>
 #include <nosync/fd-utils.h>
+#include <nosync/raw-error-result.h>
 #include <nosync/result-utils.h>
 #include <unistd.h>
 
@@ -24,16 +25,25 @@ constexpr auto max_local_buffer_size = 8192U;
 
 result<string> read_some_bytes_using_tmp_buffer(int fd, char *tmp_buffer, size_t tmp_buffer_size)
 {
+    auto read_size_res = read_some_bytes_from_fd(fd, tmp_buffer, tmp_buffer_size);
+    return read_size_res.is_ok()
+        ? make_ok_result(string(tmp_buffer, read_size_res.get_value()))
+        : raw_error_result(read_size_res);
+}
+
+}
+
+
+result<size_t> read_some_bytes_from_fd(int fd, char *buffer, size_t buffer_size)
+{
     ssize_t read_res;
     do {
-        read_res = ::read(fd, tmp_buffer, tmp_buffer_size);
+        read_res = ::read(fd, buffer, buffer_size);
     } while (read_res == -1 && errno == EINTR);
 
     return read_res >= 0
-        ? make_ok_result(string(tmp_buffer, static_cast<size_t>(read_res)))
+        ? make_ok_result(static_cast<size_t>(read_res))
         : make_raw_error_result_from_errno();
-}
-
 }
 
 
