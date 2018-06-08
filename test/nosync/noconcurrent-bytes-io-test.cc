89d59e356612fbf447edb98f11bed8626f8e79bc
Zbigniew Chyla <zbigniew.chyla@gmail.com>
2018-06-08 11:08:46 +0200

    tests: remove unused includes + using directives

--- test/nosync/noconcurrent-bytes-io-test.cc
+++ test/nosync/noconcurrent-bytes-io-test.cc
@@ -15,7 +15,6 @@ using namespace std::chrono_literals;
 using namespace std::string_literals;
 using nosync::bytes_io_mock;
 using nosync::result;
-using nosync::event_loop;
 using nosync::make_copy;
 using nosync::make_ok_result;
 using nosync::manual_event_loop;
