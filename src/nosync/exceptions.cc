addcd5350e6e5f0515e18256caa19a1f8a000cba
Zbigniew Chyla <zbigniew.chyla@gmail.com>
2018-06-08 12:12:23 +0200

    exceptions: allow building with exceptions disabled
    Use conditional compilation to handle builds with "-fno-exceptions"
    option set.
    
    The following functions change their behaviour when exceptions are
    disabled:
     - try_with_catch_all
    (call "Try" code only, "Catch" code is never called)
     - throw_logic_error,
     - throw_from_error_code,
    (print error message to stderr and abort)
    ... plus functions that use them internally.

--- src/nosync/exceptions.cc
+++ src/nosync/exceptions.cc
@@ -1,4 +1,6 @@
 // This file is part of libnosync library. See LICENSE file for license details.
+#include <cstdio>
+#include <cstdlib>
 #include <nosync/exceptions.h>
 #include <stdexcept>
 #include <string>
@@ -13,21 +15,50 @@ using std::system_error;
 namespace nosync
 {
 
+namespace
+{
+
+template<typename Exception>
+[[noreturn]] void throw_or_abort(Exception &&e);
+
+#if defined(__cpp_exceptions) && __cpp_exceptions
+
+template<typename Exception>
+void throw_or_abort(Exception &&e)
+{
+    throw e;
+}
+
+#else
+
+template<typename Exception>
+void throw_or_abort(Exception &&e)
+{
+    std::fprintf(stderr, "Trying to throw exception with exceptions disabled (%s). Aborting!", e.what());
+    std::fflush(stderr);
+    std::abort();
+}
+
+#endif
+
+}
+
+
 void throw_logic_error(const string &msg)
 {
-    throw logic_error(msg);
+    throw_or_abort(logic_error(msg));
 }
 
 
 void throw_from_error_code(error_code ec)
 {
-    throw system_error(ec);
+    throw_or_abort(system_error(ec));
 }
 
 
 void throw_from_error_code(error_code ec, const string &msg)
 {
-    throw system_error(ec, msg);
+    throw_or_abort(system_error(ec, msg));
 }
 
 }
