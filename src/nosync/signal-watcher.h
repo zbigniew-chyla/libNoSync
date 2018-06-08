2854f822b448a993489484a48f315cbe5dce857a
Zbigniew Chyla <zbigniew.chyla@gmail.com>
2018-06-08 11:08:47 +0200

    Move make_signal_watcher to its own file
    Separate make_signal_watcher code from its throwing wrapper
    (make_signalfd_watcher) by moving it to its own file.
    The old function will be deprecated.
    
    No code changes, just moving it between files.

--- src/nosync/signal-watcher.h
+++ src/nosync/signal-watcher.h
@@ -2,6 +2,19 @@
 #ifndef NOSYNC__SIGNAL_WATCHER_H
 #define NOSYNC__SIGNAL_WATCHER_H
 
-#include <nosync/signalfd-watcher.h>
+#include <functional>
+#include <nosync/fd-watching-event-loop.h>
+#include <nosync/interface-type.h>
+#include <nosync/result.h>
+#include <memory>
+
+
+namespace nosync
+{
+
+result<std::shared_ptr<interface_type>> make_signal_watcher(
+    fd_watching_event_loop &evloop, int signal_num, std::function<void()> &&signal_handler);
+
+}
 
 #endif /* NOSYNC__SIGNAL_WATCHER_H */
--- src/nosync/signalfd-watcher.cc
+++ src/nosync/signalfd-watcher.cc
@@ -1,92 +1,15 @@
 // This file is part of libnosync library. See LICENSE file for license details.
-#include <array>
-#include <csignal>
-#include <cstring>
-#include <nosync/activity-owner.h>
-#include <nosync/exceptions.h>
-#include <nosync/fd-utils.h>
 #include <nosync/result-utils.h>
-#include <nosync/shared-fd.h>
 #include <nosync/signal-watcher.h>
 #include <nosync/signalfd-watcher.h>
-#include <sys/signalfd.h>
 
-using std::array;
 using std::function;
-using std::make_shared;
 using std::move;
 using std::shared_ptr;
 
 
 namespace nosync
 {
-
-namespace
-{
-
-result<shared_fd> create_signalfd_for_signal(int signal_num)
-{
-    sigset_t sigset;
-    if (sigemptyset(&sigset) < 0 || sigaddset(&sigset, signal_num) < 0) {
-        return make_raw_error_result_from_errno();
-    }
-
-    int signalfd_retval = ::signalfd(-1, &sigset, SFD_NONBLOCK | SFD_CLOEXEC);
-    return signalfd_retval >= 0
-        ? make_ok_result(shared_fd(signalfd_retval))
-        : make_raw_error_result_from_errno();
-}
-
-
-class signalfd_watcher : public interface_type
-{
-public:
-    signalfd_watcher(fd_watching_event_loop &evloop, shared_fd &&signal_fd, function<void()> &&signal_handler);
-
-private:
-    void handle_signalfd_input();
-
-    function<void()> signal_handler;
-    shared_fd signal_fd;
-    activity_owner signalfd_watch_owner;
-};
-
-
-signalfd_watcher::signalfd_watcher(
-    fd_watching_event_loop &evloop, shared_fd &&signal_fd, function<void()> &&signal_handler)
-    : signal_handler(move(signal_handler)), signal_fd(move(signal_fd))
-{
-    signalfd_watch_owner = evloop.add_watch(
-        *this->signal_fd, fd_watch_mode::input,
-        [this]() {
-            handle_signalfd_input();
-        });
-}
-
-
-void signalfd_watcher::handle_signalfd_input()
-{
-    array<char, sizeof(::signalfd_siginfo)> siginfo_data;
-    auto read_res = read_some_bytes_from_fd(*signal_fd, siginfo_data.data(), siginfo_data.size());
-    if (read_res.is_ok() && read_res.get_value() == siginfo_data.size()) {
-        signal_handler();
-    }
-}
-
-}
-
-
-result<shared_ptr<interface_type>> make_signal_watcher(
-    fd_watching_event_loop &evloop, int signal_num, function<void()> &&signal_handler)
-{
-    auto signal_fd_res = create_signalfd_for_signal(signal_num);
-    return signal_fd_res.is_ok()
-        ? make_ok_result(
-            make_shared<signalfd_watcher>(
-                evloop, move(signal_fd_res.get_value()), move(signal_handler)))
-        : raw_error_result(signal_fd_res);
-}
-
 
 shared_ptr<interface_type> make_signalfd_watcher(
     fd_watching_event_loop &evloop, int signal_num, function<void()> &&signal_handler)
