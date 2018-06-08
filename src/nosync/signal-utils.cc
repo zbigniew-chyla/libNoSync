85fb284a5dd35baeda57b30098ada365ecee5f1c
Zbigniew Chyla <zbigniew.chyla@gmail.com>
2018-06-08 11:08:47 +0200

    signal_utils: add non-throwing variants of functions
    Transform signal_utils functions such that they return result<void>
    instead of throwing exceptions. Give transformed functions new names
    and leave very thin wrappers (just converting result<> with an error to
    an exception where needed) under old names.

--- src/nosync/signal-utils.cc
+++ src/nosync/signal-utils.cc
@@ -1,28 +1,43 @@
 // This file is part of libnosync library. See LICENSE file for license details.
 #include <csignal>
 #include <nosync/exceptions.h>
+#include <nosync/result-utils.h>
 #include <nosync/signal-utils.h>
 
 
 namespace nosync
 {
 
-void block_signal(int signal_num)
+result<void> block_signal_for_thread(int signal_num)
 {
     sigset_t sigset;
     if (::sigemptyset(&sigset) < 0 || ::sigaddset(&sigset, signal_num) < 0) {
-        throw_system_error_from_errno();
+        return make_raw_error_result_from_errno();
     }
 
-    if (sigprocmask(SIG_BLOCK, &sigset, nullptr) < 0) {
-        throw_system_error_from_errno();
-    }
+    return sigprocmask(SIG_BLOCK, &sigset, nullptr) == 0
+        ? make_ok_result()
+        : make_raw_error_result_from_errno();
+}
+
+
+result<void> ignore_signal_for_process(int signal_num)
+{
+    return ::signal(signal_num, SIG_IGN) != SIG_ERR
+        ? make_ok_result()
+        : make_raw_error_result_from_errno();
+}
+
+
+void block_signal(int signal_num)
+{
+    throw_if_error_result(block_signal_for_thread(signal_num));
 }
 
 
 void ignore_signal(int signal_num)
 {
-    ::signal(signal_num, SIG_IGN);
+    (void) ignore_signal_for_process(signal_num);
 }
 
 }
--- src/nosync/signal-utils.h
+++ src/nosync/signal-utils.h
@@ -2,9 +2,14 @@
 #ifndef NOSYNC__SIGNAL_UTILS_H
 #define NOSYNC__SIGNAL_UTILS_H
 
+#include <nosync/result.h>
+
 
 namespace nosync
 {
+
+result<void> block_signal_for_thread(int signal_num);
+result<void> ignore_signal_for_process(int signal_num);
 
 void block_signal(int signal_num);
 void ignore_signal(int signal_num);
