6b51bb13cefaa2ba80a8a5a5ed8315c8a8b8de30
Zbigniew Chyla <zbigniew.chyla@gmail.com>
2018-06-08 11:08:47 +0200

    io_utils: add non-throwing open_pipe
    Make the existing create_nonblocking_pipe function a wrapper around the
    new one.
    
    This is preparatory change for supporting nosync builds with exceptions
    disabled.

--- src/nosync/io-utils.h
+++ src/nosync/io-utils.h
@@ -4,12 +4,18 @@
 
 #include <array>
 #include <nosync/owned-fd.h>
+#include <nosync/result.h>
 
 
 namespace nosync
 {
 
 std::array<owned_fd, 2> create_nonblocking_pipe();
+
+/*!
+Create pipe to be used inside the current process (with O_CLOEXEC and O_NONBLOCK flags set).
+*/
+result<std::array<owned_fd, 2>> open_pipe();
 
 }
 
