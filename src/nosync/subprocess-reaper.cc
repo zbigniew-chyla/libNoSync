0bfbe2bcf0bc913b13a779b5204e64542fa45eb1
Zbigniew Chyla <zbigniew.chyla@gmail.com>
2018-06-08 11:08:46 +0200

    subprocess_reaper: kill usage of shared_from_this
    Simplify subprocess_reaper by removing unnecessary usage of
    enable_shared_from_this and make_weak_this_func_proxy. Signal handler
    function object passed to make_signalfd_watcher() can safely use "this"
    inside its capture list as signalfd_watcher is fully owned by the
    subprocess_reaper object and will be destroyed by its destructor.
    
    As a side effect, "throw" statement is removed from the code, which
    moves us closer to supporting nosync builds with exceptions disabled.

--- src/nosync/subprocess-reaper.cc
+++ src/nosync/subprocess-reaper.cc
@@ -1,7 +1,6 @@
 // This file is part of libnosync library. See LICENSE file for license details.
 #include <csignal>
 #include <nosync/event-loop-utils.h>
-#include <nosync/memory-utils.h>
 #include <nosync/raw-error-result.h>
 #include <nosync/requests-queue.h>
 #include <nosync/result-utils.h>
@@ -9,16 +8,13 @@
 #include <nosync/subprocess-reaper.h>
 #include <nosync/time-utils.h>
 #include <nosync/type-utils.h>
-#include <stdexcept>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <system_error>
 
 namespace ch = std::chrono;
-using std::enable_shared_from_this;
 using std::make_shared;
 using std::move;
-using std::runtime_error;
 using std::shared_ptr;
 
 
@@ -28,12 +24,10 @@ namespace nosync
 namespace
 {
 
-class subprocess_reaper : public request_handler<pid_t, int>, public enable_shared_from_this<subprocess_reaper>
+class subprocess_reaper : public request_handler<pid_t, int>
 {
 public:
     explicit subprocess_reaper(fd_watching_event_loop &evloop);
-
-    void start();
 
     void handle_request(
         pid_t &&req_pid, ch::nanoseconds timeout,
@@ -42,31 +36,19 @@ public:
 private:
     void collect_terminated_children();
 
-    fd_watching_event_loop &evloop;
     requests_queue<pid_t, int> pending_requests;
     shared_ptr<interface_type> sigchld_watcher;
 };
 
 
 subprocess_reaper::subprocess_reaper(fd_watching_event_loop &evloop)
-    : evloop(evloop), pending_requests(evloop), sigchld_watcher()
+    : pending_requests(evloop), sigchld_watcher()
 {
-}
-
-
-void subprocess_reaper::start()
-{
-    if (sigchld_watcher) {
-        throw runtime_error("subprocess reaper already started");
-    }
-
     sigchld_watcher = make_signalfd_watcher(
         evloop, SIGCHLD,
-        make_weak_this_func_proxy(
-            this,
-            [](auto &self) {
-                self.collect_terminated_children();
-            }));
+        [this]() {
+            collect_terminated_children();
+        });
 }
 
 
@@ -108,9 +90,7 @@ void subprocess_reaper::handle_request(
 
 shared_ptr<request_handler<pid_t, int>> make_subprocess_reaper(fd_watching_event_loop &evloop)
 {
-    auto reaper = make_shared<subprocess_reaper>(evloop);
-    reaper->start();
-    return reaper;
+    return make_shared<subprocess_reaper>(evloop);
 }
 
 }
