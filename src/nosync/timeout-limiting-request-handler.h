86f82e1336043dd414d570e6a149bd314576793e
Zbigniew Chyla <zbigniew.chyla@gmail.com>
2019-05-06 01:17:43 +0200

    Add timeout_limiting_request_handler

---
+++ src/nosync/timeout-limiting-request-handler.h
@@ -0,0 +1,24 @@
+// This file is part of libnosync library. See LICENSE file for license details.
+#ifndef NOSYNC__TIMEOUT_LIMITING_REQUEST_HANDLER_H
+#define NOSYNC__TIMEOUT_LIMITING_REQUEST_HANDLER_H
+
+#include <memory>
+#include <nosync/request-handler.h>
+
+
+namespace nosync
+{
+
+/*!
+Decorate request_handler<> limiting effective timeout to specified maximum value.
+*/
+template<typename Req, typename Res>
+std::shared_ptr<request_handler<Req, Res>> make_timeout_limiting_request_handler(
+    std::shared_ptr<request_handler<Req, Res>> &&base_req_handler,
+    eclock::duration max_timeout);
+
+}
+
+#include <nosync/timeout-limiting-request-handler-impl.h>
+
+#endif /* NOSYNC__TIMEOUT_LIMITING_REQUEST_HANDLER_H */
