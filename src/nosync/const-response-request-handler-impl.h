0c21daea4f047af466c70a9361735d798f9f4c78
Zbigniew Chyla <zbigniew.chyla@gmail.com>
2019-05-06 01:17:43 +0200

    Add const_response_request_handler

---
+++ src/nosync/const-response-request-handler-impl.h
@@ -0,0 +1,25 @@
+// This file is part of libnosync library. See LICENSE file for license details.
+#ifndef NOSYNC__CONST_RESPONSE_REQUEST_HANDLER_IMPL_H
+#define NOSYNC__CONST_RESPONSE_REQUEST_HANDLER_IMPL_H
+
+#include <nosync/func-request-handler.h>
+#include <nosync/result-handler-utils.h>
+#include <utility>
+
+
+namespace nosync
+{
+
+template<typename Req, typename Res>
+std::shared_ptr<request_handler<Req, Res>> make_const_response_request_handler(
+    event_loop &evloop, result<Res> &&response)
+{
+    return make_func_request_handler<Req, Res>(
+        [&evloop, response = std::move(response)](Req &&, eclock::duration, result_handler<Res> &&res_handler) {
+            invoke_result_handler_later(evloop, std::move(res_handler), response);
+        });
+}
+
+}
+
+#endif /* NOSYNC__CONST_RESPONSE_REQUEST_HANDLER_IMPL_H */
