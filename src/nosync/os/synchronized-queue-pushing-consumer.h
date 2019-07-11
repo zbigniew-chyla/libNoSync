4102ef5999178ae3f8c944f091f023d5444e8862
Zbigniew Chyla <zbigniew.chyla@gmail.com>
2019-07-12 00:08:07 +0200

    Add synchronized_queue_pushing_consumer
    Add factory function template for consumers that push consumed values
    to specified synchronized_queue<>.
    
    We'll use these consumers with new event loop type as task executors
    for passing tasks from other threads.

---
+++ src/nosync/os/synchronized-queue-pushing-consumer.h
@@ -0,0 +1,21 @@
+// This file is part of libnosync library. See LICENSE file for license details.
+#ifndef NOSYNC__OS__SYNCHRONIZED_QUEUE_PUSHING_CONSUMER_H
+#define NOSYNC__OS__SYNCHRONIZED_QUEUE_PUSHING_CONSUMER_H
+
+#include <functional>
+#include <memory>
+#include <nosync/os/synchronized-queue.h>
+
+
+namespace nosync::os
+{
+
+template<typename T>
+std::function<void(T)> make_synchronized_queue_pushing_consumer(
+    std::shared_ptr<synchronized_queue<T>> out_tasks_queue);
+
+}
+
+#include <nosync/os/synchronized-queue-pushing-consumer-impl.h>
+
+#endif /* NOSYNC__OS__SYNCHRONIZED_QUEUE_PUSHING_CONSUMER_H */
