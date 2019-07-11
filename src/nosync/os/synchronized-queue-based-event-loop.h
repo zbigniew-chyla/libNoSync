271dfb8baaec5d718ff9c5f00587d4f157b8335c
Zbigniew Chyla <zbigniew.chyla@gmail.com>
2019-07-12 00:08:07 +0200

    Add synchronized_queue_based_event_loop
    New type of event_loop that can accept tasks from other threads (via
    dedicated queue) and contains no OS-specific code.

---
+++ src/nosync/os/synchronized-queue-based-event-loop.h
@@ -0,0 +1,22 @@
+// This file is part of libnosync library. See LICENSE file for license details.
+#ifndef NOSYNC__OS__SYNCHRONIZED_QUEUE_BASED_EVENT_LOOP_H
+#define NOSYNC__OS__SYNCHRONIZED_QUEUE_BASED_EVENT_LOOP_H
+
+#include <functional>
+#include <memory>
+#include <nosync/controllable-event-loop.h>
+#include <nosync/os/synchronized-queue.h>
+
+
+namespace nosync::os
+{
+
+/*!
+Create event_loop object that can accept tasks from other threads via a queue.
+*/
+std::shared_ptr<controllable_event_loop> make_synchronized_queue_based_event_loop(
+    std::shared_ptr<synchronized_queue<std::function<void()>>> ext_tasks_queue);
+
+}
+
+#endif /* NOSYNC__OS__SYNCHRONIZED_QUEUE_BASED_EVENT_LOOP_H */
