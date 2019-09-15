2eb362cc1eee01aca11b1eb66283c69aa67d348d
Zbigniew Chyla <zbigniew.chyla@gmail.com>
2019-09-15 17:56:43 +0200

    Add mt_event_loop, use it for controllable_mt_event_loop
    We'll want to provide mt_event_loop to some applications' startup code
    (otherwise we need to provide mt executor separately).

--- src/nosync/os/controllable-mt-event-loop.h
+++ src/nosync/os/controllable-mt-event-loop.h
@@ -3,13 +3,13 @@
 #define NOSYNC__OS__CONTROLLABLE_MT_EVENT_LOOP_H
 
 #include <nosync/controllable-event-loop.h>
-#include <nosync/os/event-loop-mt-executor-factory.h>
+#include <nosync/os/mt-event-loop.h>
 
 
 namespace nosync::os
 {
 
-class controllable_mt_event_loop : public controllable_event_loop, public event_loop_mt_executor_factory
+class controllable_mt_event_loop : public controllable_event_loop, public mt_event_loop
 {
 };
 
